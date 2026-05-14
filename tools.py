8a6dd5067c5763a2dd0a8417d9fd7243687f236c
Sekitha <sekitha.senanayake@gmail.com>
2026-05-14 22:56:02 +0530

    Update tools.py

--- tools.py
+++ tools.py
@@ -7,8 +7,4 @@ load_dotenv()
 
 os.environ["EXA_API_KEY"] = os.getenv("EXA_API_KEY")
 
-exa_search_tool = EXASearchTool(
-    num_results=3,
-    include_text=False,
-    use_autoprompt=True,
-)
+exa_search_tool = EXASearchTool()
