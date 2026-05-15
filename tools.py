56bbcb084565269be21c70fc463b5a786be2a9a6
Sekitha <sekitha.senanayake@gmail.com>
2026-05-15 19:07:15 +0530

    Update tools.py

--- tools.py
+++ tools.py
@@ -8,6 +8,6 @@ load_dotenv()
 os.environ["EXA_API_KEY"] = os.getenv("EXA_API_KEY")
 
 exa_search_tool = EXASearchTool(
-    num_results = 3,
-    text_length_limit = 800,
+    num_results = 2,
+    text_length_limit = 300,
 )
